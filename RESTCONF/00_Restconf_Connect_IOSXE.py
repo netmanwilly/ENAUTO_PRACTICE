fe27f7c177ed04441f791f059b2b570c49d9c79c
Wilson Duong <wilsonduong1264@gmail.com>
2024-01-15 09:58:14 -0800

    added Meraki Practice

--- RESTCONF/00_Restconf_Connect_IOSXE.py
+++ RESTCONF/00_Restconf_Connect_IOSXE.py
@@ -1,21 +1,25 @@
 import requests
+import json
 requests.packages.urllib3.disable_warnings()
-host = "sandbox-iosxe-latest-1.cisco.com"
+host = "sbx-nxos-mgmt.cisco.com"
 user = "admin"
-password = "C1sco12345"
+password = "Admin_1234!"
 
 def main():
 
     # url string to issue GET request
-    url = "https://{h}/restconf/data/ietf-interfaces:interfaces".format(h=host)
+    url = "https://{h}/restconf/data/".format(h=host)
 
     # RESTCONF media types for REST API headers
     headers = {'Content-Type': 'application/yang-data+json',
                'Accept': 'application/yang-data+json'}
     
     response = requests.get(url, auth=(user, password), headers=headers, verify=False)
-    print(response.text)
-    print(type(response))
+    response.raise_for_status
+    if response.status_code != 204:
+        print(response)
+    else:
+        print(f"NOTHING HERE MAN THE CODE IS {response.status_code}")
 
 if __name__ == "__main__":
     main()
